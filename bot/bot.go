cd92bcee9124ae5cfb03346b7a95e9aec3c2650f
than-os <evilthanos@protonmail.com>
2018-10-23 05:45:30 +0530

    added env vars

--- bot/bot.go
+++ bot/bot.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
 	"github.com/than-os/sent-dante/models"
 	"github.com/than-os/sent-dante/utils"
 	"gopkg.in/tucnak/telebot.v2"
-	"log"
-	"net/http"
-	"strconv"
-	"time"
 )
 
 var (
-	uri string
-	text string
+	uri   string
+	text  string
 	nodes []models.TONNode
 )
 
 func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("could not read ENV VARS. Shutting Down!!! \n%v", err)
+	}
 	resp, err := http.Get("https://ton.sentinelgroup.io/all")
 	if err != nil {
 		log.Fatal(err)
@@ -26,19 +32,16 @@ func init() {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	log.Printf("%v", nodes)
-	// always respond!
 
-	uri = "https://t.me/socks?server="+ nodes[0].IPAddr + "&port=" + strconv.Itoa(nodes[0].Port) + "&user=" + nodes[0].Username + "&pass=" + nodes[0].Password
+	uri = "https://t.me/socks?server=" + nodes[0].IPAddr + "&port=" + strconv.Itoa(nodes[0].Port) + "&user=" + nodes[0].Username + "&pass=" + nodes[0].Password
 }
 
 func main() {
 
 	b, e := telebot.NewBot(telebot.Settings{
-		//Token: "632780635:AAFLAW2XDppFLTxm1qXKqN614zFEQyjV-HU", //deployed key @et_socks_bot
-		//Token: "672453988:AAGMXyZXLHVU4nh2uR5PDRNpu595Dubl-Hs", //test key http://t.me/qwerty_et_bot
-		Token: "713749545:AAFHjglnYnjBAc0wLZyPSTwuIOANm5C0sTw", //official_key http://t.me/Sentinel_SOCKS5_bot
-		Poller: &telebot.LongPoller{ Timeout: time.Second},
+		// Token:  os.getEnv("TEST_API_KEY2"),    //http://t.me/qwerty_et_bot
+		Token:  os.Getenv("TEST_API_KEY2"), //@et_socks_bot
+		Poller: &telebot.LongPoller{Timeout: time.Second},
 		//Updates: 20,
 	})
 
@@ -49,7 +52,7 @@ func main() {
 	inlineConnectBtn := telebot.InlineButton{
 		//Unique: "sad_moon",
 		Text: "Auto Select",
-		URL: uri,
+		URL:  uri,
 		//URL: "https://t.me/socks?server=185.222.24.146&port=1090&user=sentinel&pass=$entinel@12!@",
 	}
 	inlineListBtn := telebot.InlineButton{
@@ -58,7 +61,7 @@ func main() {
 	}
 	//array of array of inline buttons
 	inlineKeys := [][]telebot.InlineButton{
-		{ inlineConnectBtn },
+		{inlineConnectBtn},
 		//{ inlineListBtn },
 		//{inlineConnectBtn},
 		// ...
@@ -80,41 +83,41 @@ func main() {
 	//inlineKeys = append(inlineKeys, []telebot.InlineButton{inlineListBtn})
 	b.Handle(&replyBtn, func(m *telebot.Message) {
 		// on reply button pressed
-		uri := "https://t.me/socks?server="+ nodes[0].IPAddr + "&port=" + strconv.Itoa(nodes[0].Port) + "&user=" + nodes[0].Username + "&pass=" + nodes[0].Password
+		uri := "https://t.me/socks?server=" + nodes[0].IPAddr + "&port=" + strconv.Itoa(nodes[0].Port) + "&user=" + nodes[0].Username + "&pass=" + nodes[0].Password
 
-		b.Send(m.Sender, "Here is your auto selected SENT-TON Node", &telebot.ReplyMarkup {
+		b.Send(m.Sender, "Here is your auto selected SENT-TON Node", &telebot.ReplyMarkup{
 			OneTimeKeyboard: true,
 			InlineKeyboard: [][]telebot.InlineButton{
-				{ telebot.InlineButton{
-					URL: uri,
+				{telebot.InlineButton{
+					URL:  uri,
 					Text: "Click To Connect",
-				} },
+				}},
 			},
 		})
 
-			//b.Respond(c, &telebot.CallbackResponse{
-			//	URL: uri,
-			//	Text: "well well well",
-			//	ShowAlert: true,
-			//})
+		//b.Respond(c, &telebot.CallbackResponse{
+		//	URL: uri,
+		//	Text: "well well well",
+		//	ShowAlert: true,
+		//})
 	})
 	b.Handle(&listBtn, func(m *telebot.Message) {
 		// on reply button pressed
 		for idx, node := range nodes {
-			uri := "https://t.me/socks?server="+ node.IPAddr + "&port=" + strconv.Itoa(node.Port) + "&user=" + node.Username + "&pass=" + node.Password
+			uri := "https://t.me/socks?server=" + node.IPAddr + "&port=" + strconv.Itoa(node.Port) + "&user=" + node.Username + "&pass=" + node.Password
 			geoLocation, err := utils.GetGeoLocation(node.IPAddr)
 			if err != nil {
-				b.Send(m.Sender, "Error Occurred: " + err.Error())
+				b.Send(m.Sender, "Error Occurred: "+err.Error())
 				return
 			}
-			b.Send(m.Sender, "SENT-TON Node #" + strconv.Itoa(idx + 1), &telebot.ReplyMarkup{
+			b.Send(m.Sender, "SENT-TON Node #"+strconv.Itoa(idx+1), &telebot.ReplyMarkup{
 				ResizeReplyKeyboard: true,
 				InlineKeyboard: [][]telebot.InlineButton{
 					{
 						telebot.InlineButton{
 							//Unique: "sad_moon",
 							Text: "Connect To " + geoLocation.RegionName,
-							URL: uri,
+							URL:  uri,
 							Data: node.Username,
 						},
 					},
@@ -135,14 +138,14 @@ func main() {
 		resp, err := http.Get("https://ton.sentinelgroup.io/all")
 		if err != nil {
 			b.Respond(c, &telebot.CallbackResponse{
-				Text: err.Error(),
+				Text:      err.Error(),
 				ShowAlert: true,
 			})
 			log.Fatal(err)
 		}
 		if err := json.NewDecoder(resp.Body).Decode(&nodes); err != nil {
 			b.Respond(c, &telebot.CallbackResponse{
-				Text: err.Error(),
+				Text:      err.Error(),
 				ShowAlert: true,
 			})
 			log.Fatal(err)
@@ -151,14 +154,14 @@ func main() {
 		log.Printf("%v", nodes)
 		// always respond!
 
-		uri = "https://t.me/socks?server="+ nodes[0].IPAddr + "&port=" + strconv.Itoa(nodes[0].Port) + "&user=" + nodes[0].Username + "&pass=" + nodes[0].Password
+		uri = "https://t.me/socks?server=" + nodes[0].IPAddr + "&port=" + strconv.Itoa(nodes[0].Port) + "&user=" + nodes[0].Username + "&pass=" + nodes[0].Password
 		inlineConnectBtn.Text = nodes[0].IPAddr
 		inlineConnectBtn.URL = uri
 
 		b.Respond(c, &telebot.CallbackResponse{
 			Text: uri,
 			//URL: "https://t.me/socks?server=185.222.24.146&port=1090&user=sentinel&pass=$entinel@12!",
-			ShowAlert:true,
+			ShowAlert: true,
 		})
 	})
 	_ = newKeys
@@ -168,20 +171,20 @@ func main() {
 			return
 		}
 		for idx, node := range nodes {
-			uri := "https://t.me/socks?server="+ node.IPAddr + "&port=" + strconv.Itoa(node.Port) + "&user=" + node.Username + "&pass=" + node.Password
+			uri := "https://t.me/socks?server=" + node.IPAddr + "&port=" + strconv.Itoa(node.Port) + "&user=" + node.Username + "&pass=" + node.Password
 			geoLocation, err := utils.GetGeoLocation(node.IPAddr)
 			if err != nil {
-				b.Send(m.Sender, "Error Occurred: " + err.Error())
+				b.Send(m.Sender, "Error Occurred: "+err.Error())
 				return
 			}
-			b.Send(m.Sender, "SENT-TON Node #" + strconv.Itoa(idx + 1), &telebot.ReplyMarkup{
+			b.Send(m.Sender, "SENT-TON Node #"+strconv.Itoa(idx+1), &telebot.ReplyMarkup{
 				ResizeReplyKeyboard: true,
 				InlineKeyboard: [][]telebot.InlineButton{
 					{
 						telebot.InlineButton{
 							//Unique: "sad_moon",
 							Text: "Connect To " + geoLocation.RegionName,
-							URL: uri,
+							URL:  uri,
 							Data: node.Username,
 						},
 					},
@@ -199,7 +202,7 @@ func main() {
 
 		msg := "List of all Telegram Open Network Nodes in Sentinel dNetwork"
 		b.Send(m.Sender, msg, &telebot.ReplyMarkup{
-			ReplyKeyboard: replyKeys,
+			ReplyKeyboard:       replyKeys,
 			ResizeReplyKeyboard: true,
 		})
 	})
